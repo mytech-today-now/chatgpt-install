e44f3ff9f31adec032ea8e5993b15441a28838cf
Kyle Rode <kyle@mytech.today>
2023-03-29 09:49:08 -0500

    modified:   ps-shortcut-install.ps1

--- ps-shortcut-install.ps1
+++ ps-shortcut-install.ps1
@@ -1,28 +1,34 @@
 <#
     
-File Name: chatgpt_install.ps1
+File Name: ps-shortcut-install.ps1
 Coder: ChatGPT-4
 Designer: kyle@mytech.today
 Created: 2023-03-29
 Updated: 2023-05-02 - Version 0.6 - added support for JSON object with multiple shortcuts loaded from a URL
 
 .SYNOPSIS
-Installs the ChatGPT URL shortcut(s) on the user's desktop, with options to add an icon, by downloading the icon file and creating a shortcut to a URL defined in a JSON object.
+Installs the Windows URL shortcut(s) on the user's desktop, with options to add an icon, by downloading the icon file and creating a shortcut to a URL defined in a JSON object.
 
 .DESCRIPTION
-This script installs the ChatGPT URL shortcut(s) on the user's desktop, by downloading the icon file and creating a shortcut to a URL defined in a JSON object. The JSON object can be loaded from a URL that can be set via the command line interface. The script checks if the shortcut(s) already exist on the desktop, start menu, and taskbar, and adds them if they are missing. The icon file is downloaded and stored in the %USERPROFILE%\myTechToday\icons directory, and assigned to the shortcut(s) if specified in the JSON object.
+This script installs the Windows URL shortcut(s) on the user's desktop, by downloading the icon file and creating a shortcut to a URL defined in a JSON object. The JSON object can be loaded from a URL that can be set via the command line interface. The script checks if the shortcut(s) already exist on the desktop, start menu, and taskbar, and adds them if they are missing. The icon file is downloaded and stored in the %USERPROFILE%\myTechToday\icons directory, and assigned to the shortcut(s) if specified in the JSON object.
 
 .PARAMETER ShortcutsUrl
 The URL of the JSON object containing the shortcut(s) information. The JSON object should have the following structure:
+
+```json
 {
     "Name": "ChatGPT",
     "URL": "https://somedomain.com/thingy",
     "Icon": "https://somedomain.com/icon.ico"
 }
+```
 
 .EXAMPLE
-Install-ChatGPTShortcut -ShortcutsUrl "https://somedomain.com/chatgpt_shortcuts.json"
+
+```bash
+> ps-shortcut-install -ShortcutsUrl "https://somedomain.com/chatgpt_shortcuts.json"
 This example installs the ChatGPT URL shortcut(s) defined in the JSON object located at https://somedomain.com/chatgpt_shortcuts.json.
+```
 
 .NOTES
 - This script requires the user to have administrative privileges to create the shortcut(s) on the desktop, start menu, and taskbar, and to create the icons directory.
@@ -33,7 +39,11 @@ This example installs the ChatGPT URL shortcut(s) defined in the JSON object loc
 - The script uses the Write-Verbose and Write-Error cmdlets to provide detailed information on the script execution.
 
 .EXAMPLE
+
+```bash
 $shortcutsUrl = "https://example.com/shortcuts.json"
+```
+
 Install-ChatGPTShortcut -ShortcutsUrl $shortcutsUrl
 
 #>
